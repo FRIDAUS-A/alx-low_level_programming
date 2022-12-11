3679e95ed9af67e8ee11edc0f911f23011854a54
FRIDAUS <fridausokoya@gmail.com>
2022-12-11 20:53:57 +0100

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,20 @@
 */
 int main(void)
 {
-	int x = 0, y = 0, z = 0, b = 0, c = 0;
+	int x , y, z;
 
-	while (x < 8)
+	for (x = 0; x < 8; x++)
 	{
-		for (y = b; y < 9; y++)
+		for (y = x + 1; y < 9; y++)
 		{
-			for (z = c; z <= 9; z++)
+			for (z = y + 1; z <= 9; z++)
 			{
-				if (!( x == y || x == z || y == z))
+				if (!(x == y || x == z || y == z))
 				{
 				putchar(x + '0');
 				putchar(y + '0');
 				putchar(z + '0');
-				if (!(x == 7 && y == 8 && z == 9))
+				if (!(x == 7 && y == 8 &&  z == 9))
 				{
 					putchar(',');
 					putchar(' ');
@@ -27,8 +27,7 @@ int main(void)
 				}
 			}
 		}
-		x++;
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
