9f00f545b335f722889b076a973670704dab1929
FRIDAUS <fridausokoya@gmail.com>
2022-12-13 09:35:49 +0100

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - A function that prints 10 times the alphabet
+ *
+ * Return: Allow success
+*/
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+
+	for (i = 0; i < 9; i++)
+	{
+		while (a <= 'z')
+			_putchar(a++);
+		_putchar('\n');
+	}
+}
