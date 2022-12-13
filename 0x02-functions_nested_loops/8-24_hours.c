704a7f3a43a392a212028d686668ccc5aca325e0
FRIDAUS <fridausokoya@gmail.com>
2022-12-14 00:52:18 +0100

    TIME

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,18 +11,18 @@ void jack_bauer(void)
 	for (a = 0; a <= 23; a++)
 	{
 		for (c = 0; c <= 5;  c++)
+		{
+			for (d = 0; d <= 9; d++)
 			{
-				for (d = 0; d <= 9; d++)
-				{
 					_putchar((a / 10) + '0');
 					_putchar((a % 10) + '0');
 					_putchar(':');
 					_putchar(c + '0');
 					_putchar(d + '0');
 					_putchar('\n');
-				}
-				d = 0;
 			}
+				d = 0;
+		}
 			c = 0;
 	}
 }
