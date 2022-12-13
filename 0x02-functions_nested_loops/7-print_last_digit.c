e0ff8b3e028dc78c1f7ba905eddbb3a965955ff4
FRIDAUS <fridausokoya@gmail.com>
2022-12-13 17:36:04 +0100

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,6 +6,17 @@
 */
 int print_last_digit(int n)
 {
+	int a;
+
+	a = -1 * (n % 10);
+	if (n > 0)
+	{
 	_putchar((n % 10) + '0');
-return (n % 10);
+	return (n % 10);
+	}
+	else
+	{
+		_putchar(a + '0');
+		return (a);
+	}
 }
