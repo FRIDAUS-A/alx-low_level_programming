d589337f7d8fdb7889b26a1ac7a3e5269ca77d8e
FRIDAUS <fridausokoya@gmail.com>
2022-12-14 11:58:49 +0100

    test

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			if (n >= 10)
+			if (n >= 10 || (n <= -10 && n > -100))
 			{
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
@@ -20,7 +20,7 @@ void print_to_98(int n)
 			_putchar(' ');
 			}
 			}
-			else
+			else if ((n > 0 && n < 9) || (n < 0 && n > -10))
 			{
 				_putchar(n + '0');
 				if (n != 98)
