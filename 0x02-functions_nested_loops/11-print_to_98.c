e6e591a035ca8005e58dfa2ad59590ef9d775c68
FRIDAUS <fridausokoya@gmail.com>
2022-12-14 11:11:34 +0100

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,26 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			_putchar((n++) + '0');
+			if (n >= 10)
+			{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			if (n != 98)
+			{
 			_putchar(',');
 			_putchar(' ');
+			}
+			}
+			else
+			{
+				_putchar(n + '0');
+				if (n != 98)
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
+			}
+			n++;
 		}
 		_putchar('\n');
 	}
@@ -20,9 +37,28 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			_putchar((n--) + '0');
+			if (n >= 100)
+			{
+			_putchar((n / 100) + '0');
+			_putchar((n % 100) / 10 + '0');
+			_putchar((n % 10) + '0');
+			if (n != 98)
+			{
 			_putchar(',');
 			_putchar(' ');
+			}
+			}
+			if (n < 100)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (n != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			n--;
 		}
 		_putchar('\n');
 	}
