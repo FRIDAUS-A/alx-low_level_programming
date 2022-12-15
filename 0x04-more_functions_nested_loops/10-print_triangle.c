1a50b028460eb1d2ff819828536d5336ab0cbccd
FRIDAUS <fridausokoya@gmail.com>
2022-12-15 15:59:58 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_triangle - a function that prints a truangle
+ * @size: parameter of the function
+ * Return: Allow success
+*/
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 0; b < size - a; b++)
+		{
+			_putchar(' ');
+		}
+		for (c = 0; c < a; c++)
+		       _putchar('#');
+	_putchar('\n');	
+	}
+}
