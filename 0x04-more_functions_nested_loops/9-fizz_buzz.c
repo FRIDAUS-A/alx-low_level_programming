6265ca1e6ab96b7fdba36f5c3832c3409dca567b
FRIDAUS <fridausokoya@gmail.com>
2022-12-15 13:55:32 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,7 @@ int main(void)
 			printf("Fizz");
 		else if (a % 5 == 0 && a % 3 != 0)
 			printf("Buzz");
-		else if ( a % 3 == 0 && a % 5 == 0)
+		else if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz");
 		if (a != 100)
 			_putchar(' ');
