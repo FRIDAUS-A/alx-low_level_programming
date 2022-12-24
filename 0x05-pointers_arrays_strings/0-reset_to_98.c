c8bfa0a6ec572fc354578c927ae57edff75e460d
FRIDAUS <ayobamifridaus@gmail.com>
2022-12-24 18:38:36 +0100

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that update the value of a variable
+ * @n: parameter of the function
+ * Return: Allow success
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
