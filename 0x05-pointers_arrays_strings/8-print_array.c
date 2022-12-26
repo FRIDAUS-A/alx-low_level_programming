fff74b6369fbab4853e28a7e48d7e586eb996504
FRIDAUS <ayobamifridaus@gmail.com>
2022-12-26 11:16:00 +0100

    Arrays are not pointers mandatory

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - a function that prints n elements of an array of integers
+ * @a: first parameter
+ * @n: second parameter
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", *(a + i));
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+}
