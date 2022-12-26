e230688add72ffd04d5eeec98f55d42ef6617b85
FRIDAUS <ayobamifridaus@gmail.com>
2022-12-26 17:41:31 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two string
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: Allow success
+*/
+char *_strcat(char *dest, char *src)
+{
+	int count = 0; int len_dest = 0;
+
+	while (*dest)
+	{
+		len_dest++;
+		dest++;
+	}
+	while (*(src + count))
+	{
+		*(dest + len_dest++) = *(src + count)
+			count++;
+	}
+	*(dest + len_dest) = '\0';
+}
