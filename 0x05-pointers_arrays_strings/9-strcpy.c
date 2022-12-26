1b21e17c9aab256ac6e32a099891849a6efcadbf
FRIDAUS <ayobamifridaus@gmail.com>
2022-12-26 12:15:50 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,12 +3,13 @@
  * _strcpy - a function that copies the string pointed to by sr.
  * @dest: first parameter
  * @src: second parameter
+ * Return: Allow success
 */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src + i))
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
