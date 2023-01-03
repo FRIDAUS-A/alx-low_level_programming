ce18be494a5dc8acf80332180774121fced939ad
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-03 10:32:02 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * _strncat - a function that concatenates two string
+  * _strncpy - a function that concatenates two string
   * @dest: 1st parameter
   * @src: 2nd parameter
   * @n: third parameter
@@ -8,15 +8,16 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int count = 0;
-	
-	while (*(dest + count++)
+	int count = 0, len_src = 0;
+
+	while (*(dest + count++))
 		len_src++;
 		while (*(src + count) && count < n)
 		{
-			if (count < n)
+			if (count <= len_src)
 				*(dest + count) = *(src + count++);
 			else
-*(dest + count) = '\0';
+				dest[count] = '\0';
+		}
 	return (dest);
 }
