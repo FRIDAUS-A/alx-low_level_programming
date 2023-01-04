0b3a5eb0fe60d1a349ce5d2420b26fc792225996
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-04 06:20:06 -0800

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * is_palindrome - a function that tests for an integer
+ * is_prime_number - a function that tests for an integer
  * @n: parameter of the function
  * Return: Allow success
 */
 int is_prime_number(int n)
 {
-	if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n != 1)
+	if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n != 1 && n > 0)
 		return (1);
 	else
 		return (0);
