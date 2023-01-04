467dc2b79125f034bab27e8f408807a13d05f281
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-04 15:07:18 -0800

    Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_palindrome - a function that checks for palindrome
+ * @s: parameter of the function
+ * Return: Allow success
+*/
+int is_palindrome(char *s)
+{
+	if (*s)
+	{
+	}
+}
