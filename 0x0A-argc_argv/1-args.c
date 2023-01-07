a3831e1788a8848015790dfd90c6baeff2997b07
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-07 02:31:01 -0800

    PRINT NO OF ARGC

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Allow success
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
