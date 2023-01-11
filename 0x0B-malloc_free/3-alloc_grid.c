46c8089148d9d11e31f111c947459b0a95af3f22
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-11 17:34:34 +0100

    create array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * alloc_grid - a function that returns a pointer to a 
+ * alloc_grid - a function that returns a pointer to a
  * 2 dimensional array
  * @width: 1st parameter
  * @height: 2nd parameter
@@ -9,20 +9,21 @@
 */
 int **alloc_grid(int width, int height)
 {
-	int **ptr,i, j;
-	
+	int **ptr, i, j;
+	/* i rep width count */
+	/* j rep height count */
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = (int **)malloc(sizeof(int *) * width);
+	ptr = (int **)malloc(sizeof(int *) * height);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
+	for (j = 0; j < height; j++)
 	{
-		*(ptr + i) = (int *)malloc(sizeof(int) * height);
-		if (*(ptr + i) == NULL)
+		*(ptr + j) = (int *)malloc(sizeof(int) * width);
+		if (*(ptr + j) == NULL)
 			return (NULL);
-		for (j = 0; j < height; j++)
-			(ptr[i])[j] = 0;
+		for (i = 0; i < width; i++)
+			(ptr[j])[i] = 0;
 	}
 	return (ptr);
 }
