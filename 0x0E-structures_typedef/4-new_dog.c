8d2a1fd69cfc953dd6d0bf4246977848afa24b16
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-16 23:25:19 +0100

    free_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		index_name++;
 	while (owner[index_owner])
 		index_owner++;
-	if (name == NULL || owner == NULL || age == NULL)
+	if (name == NULL || owner == NULL || age < 0)
 		return 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
