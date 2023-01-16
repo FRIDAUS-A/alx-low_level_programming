da0e107777548e1bc903c9aa0e3bb0495a0510e7
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-16 12:33:47 +0100

    assign value to the container

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @p: pointer to the variable
+ * @name: holds the name
+ * @age: holds age
+ * @owner: holds the name of the owner
+ * Return: Allow success
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
