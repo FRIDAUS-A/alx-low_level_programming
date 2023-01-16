336b336aad984faaeab2067708888f4885ae88a2
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-16 12:18:27 +0100

    new data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a container for two string and a float
+ * @name: holds the name
+ * @age: holds the age
+ * @owner: holds owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* MAIN_H */
