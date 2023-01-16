27c582e9d3098b903dd317054c94e1824be2d82b
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-16 16:11:23 +0100

    print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - prints a struct dog
+ * @d: address of variable
+ * Return: Allow success
+*/
+void print_dog(struct dog *d)
+{
+	d = malloc(sizeof(struct dog));
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == 0)
+			printf("(nil)");
+		else
+			printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("(nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
