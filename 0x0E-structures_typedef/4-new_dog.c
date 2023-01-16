59d3bb8aec1f861e243630d4f93a3e6bf33537f9
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-16 22:58:39 +0100

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,13 +10,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
-	dog_t *str;
-	
-	ptr = malloc(sizeof(float));
+	int index_name = 0, index_owner = 0;
+
+	while (name[index_name])
+		index_name++;
+	while (owner[index_owner])
+		index_owner++;
+	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
-	str->name = name;
+	ptr->name = malloc(sizeof(char) * (index_name + 1));
+	if (ptr->name == NULL)
+	{
+		free(ptr->name);
+		return (NULL);
+	}
+	ptr->owner = malloc(sizeof(char) * (index_owner + 1));
+	if (ptr->owner == NULL)
+	{
+		free(ptr->owner);
+		return (NULL);
+	}
+	ptr->name = name;
+	ptr->owner = owner;
 	ptr->age = age;
-	str->owner = owner;
 	return (ptr);
 }
