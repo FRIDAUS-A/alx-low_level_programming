525b7f0f9c2657d763e162abadca2a6ea15648ad
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-23 09:25:51 +0100

    print_all header

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,18 @@
 #ifndef CALC_H
 #define CALC_H
 
+/**
+ * struct op - struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+*/
+typedef struct op
+{
+	char op;
+	int (*f)(int, int);
+} op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
