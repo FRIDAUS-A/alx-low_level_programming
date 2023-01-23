d267bae4081a1920bafa197b002e0939bb135829
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-23 10:41:37 +0100

    print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,84 +1,77 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - prints character
- * @input: input
- * Return: Allow success
-*/
-void print_char(va_list input)
-{
-	char letter = va_arg(input, int);
-
-	printf("%c", letter);
-}
-
-/**
- * print_int - prints integer
- * @input: input
+ * print_int - print int
+ * @input: parameter of the function
  * Return: Allow success
 */
 void print_int(va_list input)
 {
-	int num = va_arg(input, int);
-
-	printf("%i", num);
+	printf("%d", va_arg(input, int));
 }
 
 /**
- * print_float - prints float
- * @input: input
+ * print_char - print char
+ * @input - parameter
  * Return: Allow success
+*/
+void print_char(va_list input)
+{
+	printf("%c", va_arg(input, int));
+}
+
+/**
+ * print_float - print float
+ * @input: parameter
+ * @Return: Allow success
 */
 void print_float(va_list input)
 {
-	float decimal = va_arg(input, int);
-
-	printf("%f", decimal);
+	printf("%f", va_arg(input, double));
 }
 
 /**
  * print_string - print string
- * @input: input
- * Return: Allow success
+ * @input: parameter
+ * @Return: Allow success
 */
-void print_string(va_list input)
+void print_string (va_list input)
 {
-	char *str;
+	char *str = va_arg(input, char *);
 
-	str = va_arg(input, char *);
 	if (str == NULL)
 		printf("(nil)");
-	else
-		printf("%s", str);
+	printf("%s", str);
 }
 
 /**
- * print_all - a function that prints anything
- * @format: list of all type of arg passed
- * Return: Allow success
+ * print_all - print all
+ * @format: parameter that receive the format to be
+ * printed to the standard output
+ * Return: Alllow success
 */
-void print_all(const char * const format, ...)
+void print_all(const char *format, ...)
 {
-	va_list input;
-	int i = 0, j = 0;
 	new func[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string},
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
 	};
-
+	int i = 0, j;
+	va_list input;
+	
+	if (format == NULL)
+		return;
 	va_start(input, format);
-	while (format[i] && format)
+	while (*(format + i) && format)
 	{
 		j = 0;
-		while (format[i] != *(func[j].name) && j <  4)
-		{
+		while (func[j].name != format[i] && j < 4)
 			j++;
-		}
-		if (format[i] == *(func[j].name))
+		if (func[j].name == format[i])
 			func[j].ptr(input);
-		if (format[i])
+		if (format[i + 1] && func[j].name == format[i])
 			printf(", ");
 		i++;
 	}
