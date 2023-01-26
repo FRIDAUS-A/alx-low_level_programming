702add26cb1765780722fcd6c25df12c7a523925
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-26 20:49:39 +0100

    add node to start

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,8 +11,6 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	char *dup_str;
 
-	if (str == NULL)
-		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
