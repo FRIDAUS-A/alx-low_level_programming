7b2fce6fd0090d915fef9635b01ae8750d1a4ff2
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-26 19:34:42 +0100

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - a function that prints all the elements of list_t
+ * @h: head of the node
+ * Return: Allow success
+*/
+size_t print_list(const list_t *h)
+{
+	size_t len_node = 0;
+
+	while (h->next)
+	{
+		if (h->str)
+			printf("[%d] %s", h->len, h->str);
+		else
+			printf("[0] (nil)");
+		len_node++;
+	}
+	return (len_node);
+}
