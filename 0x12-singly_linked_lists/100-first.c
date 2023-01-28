6e7cdbafdc085a50a1744d640b2ab6e8a630c7c8
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-28 18:22:53 +0100

    print to stdout

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * main - Entry point
+ *
+ * Return: Allow success
+*/
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
+}
