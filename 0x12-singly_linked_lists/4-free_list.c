1c8e4d313a1f457337352452417105e538612976
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-28 18:14:07 +0100

    free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_list - a function that frees list
+ * @head: pointer to the first node
+ * Return: Allow success
+*/
+void free_list(list_t *head)
+{
+	while (h)
+	{
+		free(head);
+		h = h->next;
+	}
+}
