77149dfccda2c613655eb83717b9ce2ae2a97749
FRIDAUS <ayobamifridaus@gmail.com>
2023-01-28 18:24:52 +0100

    print to stdout

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,7 @@
  *
  * Return: Allow success
 */
-int main(void)
+void print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-	return (0);
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n")
 }
