c685a814fb3d06b9b60471698e6c07bd827d369a
FRIDAUS <ayobamifridaus@gmail.com>
2023-02-01 20:30:17 +0100

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,7 +6,7 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int count = 0;
+	unsigned int count = 0;
 	listint_t *new, *tmp;
 	
 	new = malloc(sizeof(listint_t));
