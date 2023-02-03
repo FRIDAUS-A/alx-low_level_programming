8912e63952e27af2150586e5861bd627532d7f89
FRIDAUS <ayobamifridaus@gmail.com>
2023-02-03 22:04:03 +0100

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * mul - multiply numbers recursively
- * @x: parameter to be multiplied
- * @y: number of times to be multiplied
- * Return: Allow success
-*/
-unsigned int mul(unsigned int x, unsigned int y)
-{
-	if (y == 1)
-		return (x);
-	else
-		return (x * mul(x, y - 1));
-}
-/**
- * binary_to_uint - a function that converts a binary number to an unsigned int
- * @b: pointing to a string of 0 and 1 chars
+ * binary_to_uint - covert from binary to decimal
+ * @b: binary value to be converted
  * Return: Allow success
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len_str = 0, sum = 0;
+	unsigned int len_b = 0, sum = 0, index = 0, tmp_b, num;
 
-	while (*b)
-		len_str++;
-	len_str = len_str - 1;
-	while (len_str >= 0)
+	while (b[index])
 	{
-		if (!(b[len_str] == 0 || b[len_str] == 1))
-				return (0);
-		sum = sum + b[len_str] * mul(2,len_str);
-	       len_str--;	
+		if (!(b[index] == '0' || b[index] == '1'))
+			return (0);
+		len_b++;
+		index++;
+	}
+	len_b = len_b - 1;
+	tmp_b = len_b;
+	for (index = 0; index <= len_b; index++)
+	{
+		if (b[index] == '0')
+			num = 0;
+		else if (b[index] == '1')
+			num = 1;
+		sum = sum + (num << tmp_b);
+		tmp_b--;
 	}
 	return (sum);
 }
