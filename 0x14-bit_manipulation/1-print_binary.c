491891dbdd373b5672934bb90dcedfba145e9fd7
FRIDAUS <ayobamifridaus@gmail.com>
2023-02-03 23:17:13 +0100

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_binary - prints binary representation of a number
+ * @n: number to be converted
+ * Return: Allow success
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned int num;
+
+	if ((n >> 1) >= 0)
+	{
+		num = (n >> 1);
+		if (n == 1)
+			_putchar(1 + '0');
+		else
+		{
+			print_binary(n >> 1);
+			_putchar((n - (num << 1)) + '0');
+		}
+	}
+}
