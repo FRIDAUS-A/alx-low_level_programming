c10144f40c471edec406bdcf40dc550040f7579f
FRIDAUS <ayobamifridaus@gmail.com>
2023-02-07 14:53:19 +0100

    read from file and print to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,13 +17,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 	r_open = open(filename, O_RDONLY);
+	if (r_open == -1)
+		return (0);
 	r_read = read(r_open, buffer, letters);
 	r_write = write(STDOUT_FILENO, buffer, r_read);
-	if (r_open == -1 || r_read == -1 || r_write == -1 || r_write != r_read)
-	{
-		free(buffer);
-		return (0);
-	}
 	free(buffer);
 	close(r_open);
 	return (r_write);
