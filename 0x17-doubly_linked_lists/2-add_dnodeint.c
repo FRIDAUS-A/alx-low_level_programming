a7f57d405312924e1ae8e96edf9970550c832b55
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-16 13:35:54 +0100

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,10 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
