d8100b6f345847de39d43e90ad7c37093f617023
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-16 14:57:06 +0100

    Delete node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (*h && (count < idx))
 	{
 		count++;
-		pre = tmp;
+		pre = *h;
 		*h = (*h)->next;
 	}
 	(*h)->prev = new;
