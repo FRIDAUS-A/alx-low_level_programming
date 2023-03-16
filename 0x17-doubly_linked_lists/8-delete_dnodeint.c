44756a5e919f908455456afc912fc3d2d07abe32
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-16 15:25:03 +0100

    Delete at Index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp = *head, *pre;
 
 	if (*head == NULL)
-		return (NULL);
+		return (-1);
 	while (tmp)
 	{
 		len_node++;
